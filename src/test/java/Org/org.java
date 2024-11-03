2e2a7ace0555d982ce8ded78b43bc124ef1542e6
khurshid2407 <khurshidalam2407@gmail.com>
2024-11-03 09:10:43 +0530

    second

--- src/test/java/Org/org.java
+++ src/test/java/Org/org.java
@@ -6,11 +6,20 @@ public class org {
 	
 	@Test(groups = "smoke")
 	public void test01() {
+		String browser=System.getProperty("browser");
+		String env=System.getProperty("env");
+		System.out.println(browser);
+		System.out.println(env);
 		System.out.println("Org -> Smoke testing");
 	}
 
 	@Test(groups = "regression")
 	public void test02() {
+		String browser=System.getProperty("browser");
+		String env=System.getProperty("env");
+		
+		System.out.println(browser);
+		System.out.println(env);
 		System.out.println("Org -> regression testing");
 	}
 }
